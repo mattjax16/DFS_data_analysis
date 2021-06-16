567f8aae915af152809d4056383704cc2e37c014
matt <mnbass21@colby.edu>
2021-06-15 22:59:38 -0400

    Start of pitching data analysis project

---
+++ draftkings_mlb_data_scrapper.py
@@ -0,0 +1,26 @@
+"""
+draftkings_mlb_data_scrapper.py
+
+This is going to just be the initial scrapper for draftkings_mlb_data
+
+will contain a variety of functions
+"""
+
+
+def test_scrapper():
+
+    pass
+
+
+
+
+
+
+'''
+The main function used to test functions from this python script
+'''
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
