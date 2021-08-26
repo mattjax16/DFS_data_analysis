a0521b0f27f261aea43ab7f5188257a7c5b1522a
mattjax16 <76124126+mattjax16@users.noreply.github.com>
2021-08-26 13:14:19 -0400

    cleaned up postgres_funcs.py

--- postgres_files/postgres_funcs.py
+++ postgres_files/postgres_funcs.py
@@ -2,15 +2,35 @@
 postgres_connect.py
 
 These will be scripts to work with the postgres server
+
+code gotten from: https://naysan.ca/2020/05/31/postgresql-to-pandas/
 """
 
 
 import psycopg2
-from postgres_config import config
-
 import pandas as pd
 import sys
-from postgres_config import config
+from configparser import ConfigParser
+
+
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
 
 
 def connect():
@@ -42,7 +62,7 @@ def postgresql_to_dataframe(conn, select_query, column_names):
         cursor.close()
         return 1
 
-    # Naturally we get a list of tupples
+    # Naturally we get a list of tuples
     tupples = cursor.fetchall()
     cursor.close()
 
